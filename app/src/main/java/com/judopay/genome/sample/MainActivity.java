d84b280edd515f4ecc4b2fdf9a6eb9976904a768
James Goodwin <jamesgoodwin@gmail.com>
2017-01-24 13:24:06 +0000

    Fix layout issue

--- app/src/main/java/com/judopay/genome/sample/MainActivity.java
+++ app/src/main/java/com/judopay/genome/sample/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Credentials credentials = new Credentials("<TOKEN>", "<SECRET>");
+        Credentials credentials = new Credentials("xvTWcOyTn1ib5vvR", "73a577a8dff2459c57461598046bdce22fc1aa89dec75292899a6d33f3af273c");
         final DeviceDna deviceDna = new DeviceDna(this, credentials);
 
         TextView identityScoreText = (TextView) findViewById(R.id.identity_score_text);
@@ -33,9 +33,13 @@ public class MainActivity extends AppCompatActivity {
                                 .observeOn(AndroidSchedulers.mainThread())
                                 .subscribeOn(Schedulers.io())
                                 .subscribe(json -> {
-                                            identityScoreText.setText(json.get("IdentityScore").getAsString());
-                                            lastSeenText.setText(json.get("LastSeen").getAsString());
-                                            createdAtText.setText(json.get("CreatedAt").getAsString());
+                                            String identityScore = getString(R.string.identity_score, json.get("IdentityScore").getAsString());
+                                            String lastSeen = getString(R.string.last_seen, json.get("LastSeen").getAsString());
+                                            String createdAt = getString(R.string.created_at, json.get("CreatedAt").getAsString());
+
+                                            identityScoreText.setText(identityScore);
+                                            lastSeenText.setText(lastSeen);
+                                            createdAtText.setText(createdAt);
                                         }
                                 );
                     });
